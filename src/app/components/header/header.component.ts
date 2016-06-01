f659d1025e5f152b33f277058cf93f1e7428593e
codingonHP <vishal_24_anand@hotmail.com>
2016-06-01 15:33:58 +0530

    'update'

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'header',
+    templateUrl: '../../views/header/header.component.html'
+})
+export class HeaderComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
--- src/app/root.component.ts
+++ src/app/root.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
     selector : 'ng2-app',
-    templateUrl : 'app/root.component.html'
+    templateUrl : 'app/root.component.html',
+    directives:[HeaderComponent]
 })
 export class RootComponent{
     
